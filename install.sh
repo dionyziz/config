a6cfbbddc4044e2c46f6c2908dc572ce1c9d9707
Dionysis Zindros <dionyziz@gmail.com>
2014-12-23 11:42:38 +0000

    Add installation script for dotfiles

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+all=false
+for file in $(ls -a|grep "^.[a-z]")
+do
+  yes=false
+  if [[ "$all" = false ]] 
+  then
+    read -p "Link $file? (Y)es/(N)o/(A)ll/(Q)uit? "
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      yes=true
+    fi
+    if [[ $REPLY =~ ^[Aa]$ ]]
+    then
+      yes=true
+      all=true
+    fi
+    if [[ $REPLY =~ ^[Qq]$ ]]
+    then
+      exit
+    fi
+  fi
+  if [ "$yes" = true -o "$all" = true ]
+  then
+    echo "Linking file $file"
+    ln -v -s `pwd`/$file ~
+  fi
+done
